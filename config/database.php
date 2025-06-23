381e355474662b0c86f18085fb543ffae1fe439e
Wesley Santos <wesleyrogerio77@gmail.com>
2025-06-23 12:48:00 -0300

    refactor: atualização da configuração do banco de dados para utilizar variáveis de ambiente com suporte a $_ENV

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'driver' => getenv('DB_CONNECTION') ?: 'sqlite',
-    'host' => getenv('DB_HOST') ?: 'localhost',
-    'port' => getenv('DB_PORT') ?: '3306',
-    'database' => getenv('DB_DATABASE') ?: __DIR__ . '/../database/banco.sqlite',
-    'username' => getenv('DB_USERNAME') ?: null,
-    'password' => getenv('DB_PASSWORD') ?: null,
+    'driver' => $_ENV['DB_CONNECTION'] ?? getenv('DB_CONNECTION') ?: 'sqlite',
+    'host' => $_ENV['DB_HOST'] ?? getenv('DB_HOST') ?: 'localhost',
+    'port' => $_ENV['DB_PORT'] ?? getenv('DB_PORT') ?: '3306',
+    'database' => $_ENV['DB_DATABASE'] ?? getenv('DB_DATABASE') ?: __DIR__ . '/../database/banco.sqlite',
+    'username' => $_ENV['DB_USERNAME'] ?? getenv('DB_USERNAME') ?: null,
+    'password' => $_ENV['DB_PASSWORD'] ?? getenv('DB_PASSWORD') ?: null,
     'options' => [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
